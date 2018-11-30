92568bcda1a11295ea0b685b19b9579c5cee2276
mickijames <micalynjeanette@eagles.ewu.edu>
2018-11-30 02:52:38 -0800

    Started data parsing from test array and added date calculations

---
+++ ALF_Scheduler/ALF_Scheduler/DataParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALF_Scheduler
+{
+    class DataParser
+    {
+        // the facility name, licensee, license number,unit, address?city?, zipcode, number of beds, previous full inspection, most recent full inspection, inspection results 
+        // proposed scheduling date, number of days and months between the last full inspection and the proposed scheduling date, what 15 months is from the last full inspection, 
+        // what 18 months is from the last full inspection, number of licensors needed, sample size from last full inspection
+
+        /*
+        DatesOfSOD // dates of SOD (Statement of Deficiencies report) for substantiated complaints done since the last full inspection?
+        EnforcementNotes  // any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
+        FailedFollowUp
+        Complaints  // failed follow-ups for current inspection or complaints*/
+
+        private string[] _testExcelColumns = { "facility name", "licensee name", "U", "752103", "98001", "City", "08/14/2016", "02/23/2018", "03/23/2019", "07/25/2019", "08/25/2019", "Miranda", "Yes" };
+        Facility facility = new Facility();
+
+        private void Name() {
+            facility.Name = _testExcelColumns[0];
+        }
+
+        private void Licensee() {
+            string[] licensee = _testExcelColumns[1].Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            facility.LicenseeLastName = licensee[0];
+            facility.LicenseeFirstName = licensee[1];
+        }
+
+        private void Unit() {
+            facility.Unit = Convert.ToChar(_testExcelColumns[2]);
+        }
+
+        private void LicenseNumber() {
+            facility.LicenseNumber = Convert.ToInt32(_testExcelColumns[3]);
+        }
+
+        private void ZipCode() {
+            facility.ZipCode = Convert.ToInt32(_testExcelColumns[4]);
+        }
+
+        private void City() {
+            //facility.City = _testExcelColumns[5];
+        }
+
+        private DateTime CreateDateTime(string date) {
+            string[] dateArray = date.Split('/');
+            int month = Convert.ToInt16(dateArray[0]);
+            int day = Convert.ToInt16(dateArray[1]);
+            int year = Convert.ToInt16(dateArray[2]);
+            return new DateTime(year, month, day);
+        }
+
+        private void PreviousInspection() {
+            facility.OneYearFullInspection = CreateDateTime(_testExcelColumns[6]);
+        }
+
+        private void LastInspection() {
+            facility.LastFullInspection = CreateDateTime(_testExcelColumns[7]);
+        }
+
+        private void ProposedDate() {
+            facility.ProposedDate = CreateDateTime(_testExcelColumns[7]);
+        }
+
+        private void NumberOfBeds() {
+            facility.NumberOfBeds = Convert.ToInt16(_testExcelColumns[8]);
+        }
+    }
+}
--- ALF_Scheduler/ALF_Scheduler/Facility.cs
+++ ALF_Scheduler/ALF_Scheduler/Facility.cs
@@ -13,9 +13,16 @@ namespace ALF_Scheduler
     class Facility
     {
         public string Name { get; set; }
-        public string NameOfLicensee { get; set; }
-        public char Unit { get; set; }
+        public string NameOfLicensee {
+            get {
+                FormattableString formattableString = $"{LicenseeLastName}, {LicenseeFirstName}";
+                return formattableString.ToString();
+            }
+        }
+        public string LicenseeFirstName { get; set; }
+        public string LicenseeLastName { get; set; }
         public int LicenseNumber { get; set; }
+        public char Unit { get; set; }
         public string Address { get; set; }
         public int ZipCode { get; set; }
         public int NumberOfBeds { get; set; }
@@ -23,12 +30,17 @@ namespace ALF_Scheduler
         public DateTime OneYearFullInspection { get; set; } //need to have better names for previous inspection dates and enforcement
         public DateTime TwoYearFullInspection { get; set; } //need to have better names for previous inspection dates and enforcement
         public string InspectionResults { get; set; } //TODO connect inspection results with config file (NO, NO24, ENF, YES) 
-        public DateTime[] DatesOfSOD { get; set; } //TODO dates of SOD for substantiated complaints done since the last full inspection?
-        public string Enforcement { get; set; } //TODO any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
+        public DateTime[] DatesOfSOD { get; set; } //TODO dates of SOD (Statement of Deficiencies report) for substantiated complaints done since the last full inspection?
+        public string EnforcementNotes { get; set; } // any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
         public bool FailedFollowUp { get; set; } //datatype for this?
         public string Complaints { get; set; } // failed follow-ups for current inspection or complaints
         public DateTime ProposedDate { get; set; }
-        public float ScheduleInterval { get; set; } //time between last full inspection and proposed date
+        public float ScheduleInterval {
+            get {
+                TimeSpan difference = ProposedDate.Subtract(LastFullInspection);
+                return (float)(Convert.ToDouble(difference)*30.42); // difference in days times avg months in a year
+            }
+        } //time between last full inspection and proposed date
         public DateTime Month15 { get => LastFullInspection.AddMonths(15); } //what 15 months is from the last full inspection
         public DateTime Month18 { get => LastFullInspection.AddMonths(18); } //what 18 months is from the last full inspection
         public int NumberOfLicensors { get; set; } //number of licensors needed.. for inspection I'm guessing?
