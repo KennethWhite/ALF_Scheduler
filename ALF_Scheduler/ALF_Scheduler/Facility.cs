2ca9c155c80aef91076ae853b851e3a79a920860
mickijames <micalynjeanette@eagles.ewu.edu>
2018-11-29 16:33:26 -0800

    Simplified property formatting

--- ALF_Scheduler/ALF_Scheduler/Facility.cs
+++ ALF_Scheduler/ALF_Scheduler/Facility.cs
@@ -12,54 +12,26 @@ namespace ALF_Scheduler
 {
     class Facility
     {
-        private string name;
-        private string nameOfLicensee;
-        private char unit;
-        private int licenseNumber;
-        private string address;
-        private int zipCode;
-        private int numberOfBeds;
-        private DateTime lastFullInspection;
-        private DateTime oneYearFullInspection; //need to have better names for previous inspection dates and enforcement
-        private DateTime twoYearFullInspection; //need to have better names for previous inspection dates and enforcement
-        private string inspectionResults; //TODO connect inspection results with config file (NO, NO24, ENF, YES) 
-        private DateTime[] datesofSOD; //TODO dates of SOD for substantiated complaints done since the last full inspection?
-        private string enforcement; //TODO any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
-        private bool failedFollowUp; //datatype for this?
-        private string complaints; // failed follow-ups for current inspection or complaints
-        private DateTime proposedDate;
-        private float scheduleInterval;
-        private float month15; //what 15 months is from the last full inspection
-        private float month18; //what 18 months is from the last full inspection
-        private int numberOfLicensors; //number of licensors needed.. for inspection I'm guessing?
-        private int sampleSize; //sample size from last full inspection.. of inspectors?
-
-        public string Name { get => name; set => name = value; }
-        public string NameOfLicensee { get => nameOfLicensee; set => nameOfLicensee = value; }
-        public char Unit { get => unit; set => unit = value; }
-        public int LicenseNumber { get => licenseNumber; set => licenseNumber = value; }
-        public string Address { get => address; set => address = value; }
-        public int ZipCode { get => zipCode; set => zipCode = value; }
-        public int NumberOfBeds { get => numberOfBeds; set => numberOfBeds = value; }
-        public DateTime LastFullInspection { get => lastFullInspection; set => lastFullInspection = value; }
-        public DateTime OneYearFullInspection { get => oneYearFullInspection; set => oneYearFullInspection = value; }
-        public DateTime TwoYearFullInspection { get => twoYearFullInspection; set => twoYearFullInspection = value; }
-        public string InspectionResults { get => inspectionResults; set => inspectionResults = value; }
-        public DateTime[] DatesofSOD { get => datesofSOD; set => datesofSOD = value; }
-        public string Enforcement { get => enforcement; set => enforcement = value; }
-        public bool FailedFollowUp { get => failedFollowUp; set => failedFollowUp = value; }
-        public string Complaints { get => complaints; set => complaints = value; }
-        public DateTime ProposedDate { get => proposedDate; set => proposedDate = value; }
-        public float ScheduleInterval { get => scheduleInterval; set => scheduleInterval = value; }
-        public float Month15 { get => month15; }
-        public float Month18 { get => month18; }
-        public int NumberOfLicensors { get => numberOfLicensors; set => numberOfLicensors = value; }
-        public int SampleSize { get => sampleSize; set => sampleSize = value; }
+        public string Name { get; set; }
+        public string NameOfLicensee { get; set; }
+        public char Unit { get; set; }
+        public int LicenseNumber { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public int NumberOfBeds { get; set; }
+        public DateTime LastFullInspection { get; set; }
+        public DateTime OneYearFullInspection { get; set; } //need to have better names for previous inspection dates and enforcement
+        public DateTime TwoYearFullInspection { get; set; } //need to have better names for previous inspection dates and enforcement
+        public string InspectionResults { get; set; } //TODO connect inspection results with config file (NO, NO24, ENF, YES) 
+        public DateTime[] DatesOfSOD { get; set; } //TODO dates of SOD for substantiated complaints done since the last full inspection?
+        public string Enforcement { get; set; } //TODO any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
+        public bool FailedFollowUp { get; set; } //datatype for this?
+        public string Complaints { get; set; } // failed follow-ups for current inspection or complaints
+        public DateTime ProposedDate { get; set; }
+        public float ScheduleInterval { get; set; } //time between last full inspection and proposed date
+        public float Month15 { get; } //what 15 months is from the last full inspection
+        public float Month18 { get; } //what 18 months is from the last full inspection
+        public int NumberOfLicensors { get; set; } //number of licensors needed.. for inspection I'm guessing?
+        public int SampleSize { get; set; } //sample size from last full inspection.. of inspectors?
     }
 }
-
-/*
-5. Facility    - Micki
-    a. Standard format, in email from Lisa    
-    b. Entering Data
-*/
