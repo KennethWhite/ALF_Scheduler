9c783c1393ad18ea19f0405be1ae24d3b12d9112
mickijames <micalynjeanette@eagles.ewu.edu>
2018-12-13 04:41:50 -0800

    In-depth documentation and reworking data types and methods

--- ALF_Scheduler/ALF_Scheduler/DataParser.cs
+++ ALF_Scheduler/ALF_Scheduler/DataParser.cs
@@ -5,34 +5,45 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ALF_Scheduler
-{
+namespace ALF_Scheduler {
+
+    /// <summary>
+    /// This <c>DataParser</c> class verifies and parses the incoming data to be 
+    /// sent to the <see cref="Facility"/> object.
+    /// </summary>
+    /// <remarks>Each method takes in a string (or strings) as parameters.</remarks>
     class DataParser
     {
-        // the facility name, licensee, license number,unit, address?city?, zipcode, number of beds, previous full inspection, most recent full inspection, inspection results 
-        // proposed scheduling date, number of days and months between the last full inspection and the proposed scheduling date, what 15 months is from the last full inspection, 
-        // what 18 months is from the last full inspection, number of licensors needed, sample size from last full inspection
+        Facility facility;
 
-        /*
-        DatesOfSOD // dates of SOD (Statement of Deficiencies report) for substantiated complaints done since the last full inspection?
-        EnforcementNotes  // any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
-        FailedFollowUp
-        Complaints  // failed follow-ups for current inspection or complaints*/
-
-        Facility facility = new Facility();
-
-        public Facility GetFacility() {
-            return facility;
+        /// <summary>
+        /// The DataParser constructor for a new facility.
+        /// </summary>
+        public DataParser() {
+            facility = new Facility();
         }
 
+        /// <summary>
+        /// The DataParser constructor for a specific facility.
+        /// </summary>
+        /// <param name="fac">The facility object to be used.</param>
+        public DataParser(Facility fac) {
+            facility = fac;
+        }
+
+        /// <summary>
+        /// This method creates the facility <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">The facility's name as a string.</param>
         public void Name(string name) {
             facility.Name = name;
         }
-        
+
         /// <summary>
-        /// This method will split the licensee's name into first and last.
+        /// This method will split the <paramref name="licensee"/>'s name into first and last 
+        /// and place them into their properties.
         /// </summary>
-        /// <param name="licensee">The licensee's full name separated by a comma</param>
+        /// <param name="licensee">The licensee's full name as a string separated by a comma.</param>
         public void Licensee(string licensee) {
             string[] firstLast = licensee.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
             facility.LicenseeLastName = firstLast[0];
@@ -40,37 +51,97 @@ namespace ALF_Scheduler
         }
 
         /// <summary>
-        /// This method place the licensee's first and last names into their properties.
+        /// This method place the licensee's <paramref name="first"/> and 
+        /// <paramref name="last"/> names into their properties.
         /// </summary>
-        /// <param name="licensee">The licensee's first and last names, respectively</param>
+        /// <param name="first">The licensee's first name as a string.</param>
+        /// <param name="last">The licensee's last name as a string.</param>
         public void Licensee(string first, string last) {
             facility.LicenseeFirstName = first;
             facility.LicenseeLastName = last;
         }
 
+        // TODO should this be a char or a number or allow for both?
+        /// <summary>
+        /// This method converts the facility <paramref name="unit"/> to a 
+        /// char for the facility object.
+        /// </summary>
+        /// <param name="unit">The facility's unit as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot 
+        /// be converted to a char.</exception>
         public void Unit(string unit) {
-            facility.Unit = Convert.ToChar(unit);
+            try {
+                facility.Unit = Convert.ToChar(unit);
+            } catch (Exception e) {
+                Console.Write("{0} exception in unit", e);
+                throw new InvalidCastException("unit string was unable to be converted to a char");
+            }
         }
 
+        /// <summary>
+        /// This method creates the facility's license <paramref name="number"/>.
+        /// </summary>
+        /// <param name="number">The facility's license number as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot
+        /// be converted to an int.</exception>
         public void LicenseNumber(string number) {
-            facility.LicenseNumber = Convert.ToInt32(number);
+            try {
+                facility.LicenseNumber = Convert.ToInt32(number);
+            } catch (Exception e) {
+                Console.Write("{0} exception in license number", e);
+                throw new InvalidCastException("license number string was unable to be converted to an int");
+            }
         }
 
+        /// <summary>
+        /// This method creates the facility <paramref name="zip"/>code.
+        /// </summary>
+        /// <param name="zip">The facility's zipcode as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot
+        /// be converted to an int.</exception>
         public void ZipCode(string zip) {
-            facility.ZipCode = Convert.ToInt32(zip);
+            try {
+                facility.ZipCode = Convert.ToInt16(zip);
+            } catch (Exception e) {
+                Console.Write("{0} exception in zipcode", e);
+                throw new InvalidCastException("zipcode string was unable to be converted to an int");
+            }
         }
 
-        public void Address(string address) {
-            facility.Address = address;
+        /// <summary>
+        /// This method creates the number of <paramref name="beds"/> in the facility.
+        /// </summary>
+        /// <param name="beds">The number of beds in the facility as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot 
+        /// be converted to an int.</exception>
+        public void NumberOfBeds(string beds) {
+            try {
+                facility.NumberOfBeds = Convert.ToInt16(beds);
+            } catch (Exception e) {
+                Console.Write("{0} exception in number of beds", e);
+                throw new InvalidCastException("string of beds was unable to be converted to an int");
+            }
+        }
+
+        /// <summary>
+        /// This method creates the facility <paramref name="city"/>.
+        /// </summary>
+        /// <param name="city">The city the facility resides in as a string.</param>
+        public void City(string city) {
+            facility.City = city;
         }
 
         /// <summary>
         /// This method will check the incoming string against the regex
-        /// pattern for dates, then create a DateTime object from the string.
+        /// pattern for dates, then create a DateTime object from the <paramref name="date"/>.
         /// </summary>
-        /// <param name="date">The date to be created</param>
+        /// <remarks>
+        /// <para>The date can be in the form of MM/DD/YY or MM/DD/YYYY.</para>
+        /// <para>The month can be with or without a leading zero (03 or just 3 is fine).</para>
+        /// </remarks>
+        /// <param name="date">The date to be created as a string.</param>
         /// <returns>DateTime object</returns>
-        /// <exception>Throws FormatException if date is not in the correct format</exception>
+        /// <exception cref="FormatException">Throws FormatException if date is not in the correct format.</exception>
         public static DateTime CreateDateTime(string date) {
             Regex rx = new Regex("([0-9]|[0-9]{2})/([0-9]{2})/([0-9]{4}|[0-9]{2})");
             if (rx.IsMatch(date)) {
@@ -80,20 +151,139 @@ namespace ALF_Scheduler
             }
         }
 
-        public void PreviousInspection(string date) {
-            facility.LastFullInspection = CreateDateTime(date);
-        }
-
-        public void LastInspection(string date) {
+        /// <summary>
+        /// This method creates the facility's most recent inspection <paramref name="date"/> 
+        /// by using the CreateDateTime method here: <see cref="DataParser.CreateDateTime(string date)"/>.
+        /// </summary>
+        /// <param name="date">The facility's most recent inspection date as a string.</param>
+        public void MostRecentInspection(string date) {
             facility.MostRecentFullInspection = CreateDateTime(date);
         }
 
+        /// <summary>
+        /// This method creates the facility's one year inspection <paramref name="date"/> 
+        /// by using the CreateDateTime method here: <see cref="DataParser.CreateDateTime(string date)"/>.
+        /// </summary>
+        /// <param name="date">The facility's one year inspection date as a string.</param>
+        public void OneYearInspection(string date) {
+            facility.OneYearFullInspection = CreateDateTime(date);
+        }
+
+        /// <summary>
+        /// This method creates the facility's two year inspection <paramref name="date"/> 
+        /// by using the CreateDateTime method here: <see cref="DataParser.CreateDateTime(string date)"/>.
+        /// </summary>
+        /// <param name="date">The facility's two year inspection date as a string.</param>
+        public void TwoYearInspection(string date) {
+            facility.TwoYearFullInspection = CreateDateTime(date);
+        }
+
+        //TODO connect to config file
+        /// <summary>
+        /// This method sets the inspection <paramref name="result"/> from the result code config file.
+        /// </summary>
+        /// <param name="result">The inspection result as a string.</param>
+        public void InspectionResult(string result) {
+            if (true) { 
+                facility.InspectionResult = result;
+            } else {
+                // create new code
+            }
+        }
+
+        //TODO dates of SOD (Statement of Deficiencies report)
+        /// <summary>
+        /// This method creates a DateTime array for <paramref name="dates"/> of substantiated complaints done since the last full inspection.
+        /// </summary>
+        /// <param name="dates">The array of dates.</param>
+        public void DatesOfSOD(string[] dates) {
+            try {
+                DateTime[] SODs = new DateTime[dates.Length];
+                for (int x = 0; x < dates.Length; x++) {
+                    SODs[x] = CreateDateTime(dates[x]);
+                }
+            } catch (Exception e) {
+                Console.Write("{0} exception in dates of SOD", e);
+            }
+        }
+
+        //TODO Complaints
+        /// <summary>
+        /// This method creates a string for <paramref name="complaints"/> made since last full inspection.
+        /// </summary>
+        /// <param name="complaints">The complaints as a string.</param>
+        public void Complaints(string complaints) {
+
+        }
+
+        //TODO Enforcement Notes
+        /// <summary>
+        /// This method creates a string for any enforcement <paramref name="notes"/> for any inspection done since the last full inspection 
+        /// (fines, stop placement, conditions, revocation, summary suspension).
+        /// </summary>
+        /// <param name="notes">The enforcement notes as a string.</param>
+        public void EnforcementNotes(string notes) {
+            facility.EnforcementNotes = notes;
+        }
+
+        //TODO determine best way to do Failed Follow Up
+        /// <summary>
+        /// This method creates a DateTime array of dates from failed follow ups from the most recent inspection or complaint based follow ups.
+        /// </summary>
+        /// <param name="dates">The failed follow up dates as a string array.</param>
+        public void FailedFollowUp(string[] dates) {
+            try {
+                DateTime[] followUps = new DateTime[dates.Length];
+                for (int x = 0; x < dates.Length; x++) {
+                    followUps[x] = CreateDateTime(dates[x]);
+                }
+            } catch (Exception e) {
+                Console.Write("{0} exception in failed follow up", e);
+            }
+        }
+
+        /// <summary>
+        /// This method creates the facility's proposed inspection <paramref name="date"/> by using the CreateDateTime method here: <see cref="DataParser.CreateDateTime(string date)"/>.
+        /// </summary>
+        /// <param name="date">The facility's proposed inspection date as a string.</param>
         public void ProposedDate(string date) {
             facility.ProposedDate = CreateDateTime(date);
         }
 
-        public void NumberOfBeds(string beds) {
-            facility.NumberOfBeds = Convert.ToInt16(beds);
+        /// <summary>
+        /// This method creates the <paramref name="number"/> of licensors needed for inspections based on bed count.
+        /// </summary>
+        /// <param name="number">The number of beds in the facility as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot be converted to an int.</exception>
+        public void NumberOfLicensors(string number) {
+            try {
+                facility.NumberOfLicensors = Convert.ToInt16(number);
+            } catch (Exception e) {
+                Console.Write("{0} exception in number of licensors", e);
+                throw new InvalidCastException("string of licensors was unable to be converted to an int");
+            }
+        }
+
+        /// <summary>
+        /// This method creates the sample <paramref name="size"/> of licensors on the last inspection.
+        /// </summary>
+        /// <param name="size">The sample size of inspectors as a string.</param>
+        /// <exception cref="InvalidCastException">Throws InvalidCastException if the string cannot be converted to an int.</exception>
+        public void SampleSize(string size) {
+            try {
+                facility.SampleSize = Convert.ToInt16(size);
+            } catch (Exception e) {
+                Console.Write("{0} exception in sample size", e);
+                throw new InvalidCastException("string of sample size was unable to be converted to an int");
+            }
+        }
+
+        /// <summary>
+        /// This method creates the facility's special <paramref name="info"/> column for items such as memory care and mental health.
+        /// </summary>
+        /// <param name="info">The facility's special information as a string.</param>
+        public void SpecialInfo(string info) {
+            facility.SpecialInfo = info;
         }
     }
 }
--- ALF_Scheduler/ALF_Scheduler/Facility.cs
+++ ALF_Scheduler/ALF_Scheduler/Facility.cs
@@ -5,45 +5,104 @@ using System.Text;
 using System.Threading.Tasks;
 
 /// <summary>
-/// This class is used to store the metadata in properties for each facility
+/// This class is used to store the metadata in properties for each facility.
 /// </summary>
 
 namespace ALF_Scheduler
 {
-    class Facility
-    {
+    class Facility {
+
+        /// <value>Gets the Facility name.</value>
         public string Name { get; set; }
+
+        /// <value>Gets the full name of the licensee separated by a comma (last, first).</value>
         public string NameOfLicensee {
             get {
                 FormattableString formattableString = $"{LicenseeLastName}, {LicenseeFirstName}";
                 return formattableString.ToString();
             }
         }
+
+        /// <value>Gets the Licensee's first name.</value>
         public string LicenseeFirstName { get; set; }
+
+        /// <value>Gets the Licensee's last name.</value>
         public string LicenseeLastName { get; set; }
+
+        /// <value>Gets the license number.</value>
         public int LicenseNumber { get; set; }
+
+        /// <value>Gets the Facility's unit.</value>
         public char Unit { get; set; }
-        public string Address { get; set; }
+
+        /// <value>Gets the Facility's address.</value>
+        public string City { get; set; }
+
+        /// <value>Gets the Facility's zipcode.</value>
         public int ZipCode { get; set; }
+
+        /// <value>Gets the number of beds in the facility.</value>
         public int NumberOfBeds { get; set; }
+
+        /// <value>Gets the Facility's most recent full inspection date.</value>
         public DateTime MostRecentFullInspection { get; set; }
-        public DateTime LastFullInspection { get; set; }
-        public DateTime PreviousFullInspection { get; set; }
-        public string InspectionResults { get; set; } //TODO connect inspection results with config file (NO, NO24, ENF, YES) 
-        public DateTime[] DatesOfSOD { get; set; } //TODO dates of SOD (Statement of Deficiencies report) for substantiated complaints done since the last full inspection?
-        public string EnforcementNotes { get; set; } // any enforcement for any inspection done since the last full inspection? (fines, stop placement, conditions, revocation, summary suspension)
-        public bool FailedFollowUp { get; set; } //datatype for this?
-        public string Complaints { get; set; } // failed follow-ups for current inspection or complaints
+
+        /// <value>Gets the Facility's full inspection date from a year ago.</value>
+        public DateTime OneYearFullInspection { get; set; }
+
+        /// <value>Gets the Facility's full inspection date from two years ago.</value>
+        public DateTime TwoYearFullInspection { get; set; }
+
+        // TODO Question: should we combine all the dates with their inspection results 
+        // and notes for quick access for comparison, or keep them like this and just look 
+        // back at old reports to see the codes?
+
+        //TODO connect inspection results with config file (NO, NO24, ENF, YES) 
+        /// <value>Gets the inspection result for the facility.</value>
+        public string InspectionResult { get; set; }
+
+        //TODO dates of SOD (Statement of Deficiencies report)
+        /// <value>Gets the dates of SOD (statement of deficiencies report) for complaints done since the last inspection.</value>
+        public DateTime[] DatesOfSOD { get; set; }
+
+        // TODO enforcement notes
+        /// <value>Gets the Facility's enforcement notes (fines, stop placement, conditions, revocation, summary suspension) since last inspection.</value>
+        public string EnforcementNotes { get; set; }
+
+        // TODO failed follow up. Datatype for this??
+        /// <value>Gets the value for failed follow ups.</value>
+        public DateTime[] FailedFollowUp { get; set; }
+
+        // TODO complaints
+        /// <value>Gets the Facility's complaints.</value>
+        public string Complaints { get; set; }
+
+        /// <value>Gets the proposed inspection date.</value>
         public DateTime ProposedDate { get; set; }
-        public float ScheduleInterval { //time between last full inspection and proposed date
+
+        /// <value>Gets the time interval between the most recent inspection, and the proposed date.</value>
+        /// <remarks>The difference in time is calculated by multiplying the difference in days by the average months in a year.</remarks>
+        public float ScheduleInterval {
             get {
-                TimeSpan difference = ProposedDate.Subtract(LastFullInspection);
-                return (float)(Convert.ToDouble(difference)*30.42); // difference in days times avg months in a year
+                TimeSpan difference = ProposedDate.Subtract(MostRecentFullInspection);
+                return (float)(Convert.ToDouble(difference)*30.42);
             }
         }
-        public DateTime Month15 { get => LastFullInspection.AddMonths(15); }
-        public DateTime Month18 { get => LastFullInspection.AddMonths(18); }
-        public int NumberOfLicensors { get; set; } //number of licensors needed.. for inspection I'm guessing?
-        public int SampleSize { get; set; } //sample size from last full inspection.. of inspectors?
+
+        /// <value>Gets the date 15 months from the most recent inspection.</value>
+        public DateTime Month15 { get => MostRecentFullInspection.AddMonths(15); }
+
+        /// <value>Gets the cutoff date 18 months from the most recent inspection.</value>
+        public DateTime Month18 { get => MostRecentFullInspection.AddMonths(18); }
+
+        // TODO double check this is what's meant by number of licensors
+        /// <value>Gets the number of licensors needed based on bed count for this facility's inspection.</value>
+        public int NumberOfLicensors { get; set; }
+        
+        /// <value>Gets the sample size of inspectors from the most recent inspection.</value>
+        public int SampleSize { get; set; }
+
+        /// <value>Gets any special information listed for the facility.</value>
+        public string SpecialInfo { get; set; }
     }
 }
