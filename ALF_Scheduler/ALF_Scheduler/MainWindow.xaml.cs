3c5098d2b88ca871d12789f7db39417b4be3cdc6
mickijames <micalynjeanette@eagles.ewu.edu>
2019-01-18 16:13:42 -0800

    + Added more automation in shown details + More testing information and formatting [ ] Need to add scrolling function and fix textbox info

--- ALF_Scheduler/ALF_Scheduler/MainWindow.xaml.cs
+++ ALF_Scheduler/ALF_Scheduler/MainWindow.xaml.cs
@@ -20,15 +20,25 @@ namespace ALF_Scheduler
     /// </summary>
     public partial class MainWindow : Window
     {
+        Facility fac1;
+
         public MainWindow() {
             InitializeComponent();
             List<Facility> items = new List<Facility>();
 
-            //TODO connect Facility DB for display
-            Facility fac1 = new Facility();
+            // TODO connect Facility DB for display
+            fac1 = new Facility();
             DataParser dp = new DataParser(fac1);
-            dp.Name("Facility 1");
-            dp.MostRecentInspection("01/12/2019");
+            dp.Name("Lakeland Adult Family Home");
+            dp.Licensee("Mendez, Catherine");
+            dp.Unit("G");
+            dp.LicenseNumber("725103");
+            dp.ZipCode("98001");
+            dp.City("Algona");
+            dp.OneYearInspection("08/14/2015");
+            dp.MostRecentInspection("02/23/2018");
+            dp.NumberOfLicensors("2");
+            dp.InspectionResult("NO");
             dp.ProposedDate("03/22/2020");
             items.Add(dp.GetFacility());
             
@@ -36,6 +46,7 @@ namespace ALF_Scheduler
             dp = new DataParser(fac2);
             dp.Name("Facility 2");
             dp.MostRecentInspection("08/25/2017");
+            dp.InspectionResult("yes");
             dp.ProposedDate("03/02/2019");
             items.Add(dp.GetFacility());
 
@@ -43,11 +54,13 @@ namespace ALF_Scheduler
             dp = new DataParser(fac3);
             dp.Name("Facility 3");
             dp.MostRecentInspection("12/05/2017");
+            dp.InspectionResult("enf");
             dp.ProposedDate("05/14/2019");
             items.Add(dp.GetFacility());
 
             FacilityList.ItemsSource = items;
             AddSelectedDates(items);
+            DetailsInit();
         }
 
         /// <summary>
@@ -62,7 +75,54 @@ namespace ALF_Scheduler
 
         private void SearchButton_Click(object sender, RoutedEventArgs e) {
             string facility = SearchText.Text;
-            // TODO search for facility based on text content ^
+            // TODO search for facility based on text content ^ and open it in details page
+            TabItemDetails.IsSelected = true;
+        }
+
+        // TODO formatting and add ability to scroll
+        private void DetailsInit() {
+            Grid grid = DetailsGrid;
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            string[] labelContent = { "Facility Name", "Name of Licensee", "License Number", "Unit", "City", "ZipCode", "Number Of Beds", "Most Recent Full Inspection",
+                    "One Year Full Inspection", "Two Year Full Inspection", "Inspection Result", "Dates Of SOD", "Enforcement Notes", "Failed Follow Up", "Complaints",
+                    "Proposed Date", "Schedule Interval", "Month 15", "Month 18", "Number Of Licensors", "Sample Size", "Special Info" };
+            
+            for (int x = 0; x < labelContent.Length; x++) {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = GridLength.Auto;
+                grid.RowDefinitions.Add(rowDefinition);
+                Label label = new Label();
+                label.Content = labelContent[x];
+                
+                Grid.SetColumn(label, 0);
+                Grid.SetRow(label, x);
+                grid.Children.Add(label);
+            }
+            
+            TabItemDetails.Content = grid;
+        }
+
+        private void OpenDetails() {
+            for (int row = 0; row < DetailsGrid.RowDefinitions.Count; row++) {
+                TextBox textbox = new TextBox();
+                textbox.Height = 15;
+                textbox.IsReadOnly = true;
+                // TODO connect to facility
+                textbox.Text = fac1.Name;
+                Grid.SetColumn(textbox, 1);
+                Grid.SetRow(textbox, row);
+                DetailsGrid.Children.Add(textbox);
+            }
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (TabItemFacilities.IsSelected) {
+                Console.WriteLine("tab control selection changed" + TabItemFacilities);
+            } else if (TabItemDetails.IsSelected) {
+                Console.WriteLine("tab control selection changed" + TabItemDetails);
+                OpenDetails();
+            }
         }
     }
 }
