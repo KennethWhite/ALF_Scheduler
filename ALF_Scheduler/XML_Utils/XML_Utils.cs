2a5678629f3b6d75723e65b082f20d5c380df607
Colton Johansson <cjohansson@eagles.ewu.edu>
2019-01-25 18:14:55 -0800

    Added some more methods. Switch from using XmlDocument to XDocument

--- ALF_Scheduler/XML_Utils/XML_Utils.cs
+++ ALF_Scheduler/XML_Utils/XML_Utils.cs
@@ -10,15 +10,24 @@ namespace ALF_Mine
     {
         public static void Main()
         {
-            createInitCodeXML(@".\import\codes.xml");
-            loadFiles();
+            CreateInitFolders();
+            CreateInitCodeXml(@".\default\Default_Codes.xml");
+            XDocument[] docAra = LoadFiles();
+            ReadCodeXml(docAra[0]);
         }
 
-        private static void readXML(String path)
-        {
-            //This was just for testing mostly, but once we get a file tree set up, we can have it point to our program settings XML
-            XDocument doc = XDocument.Load(path);
 
+
+        private static void ReadCodeXml(String path)
+        {
+            XmlDocument temp = new XmlDocument();
+            temp.Load(path);
+            XDocument doc = XDocument.Parse(temp.OuterXml);
+            ReadCodeXml(doc);
+        }
+
+        private static void ReadCodeXml(XDocument doc)
+        {
             var codes = from c in doc.Descendants("code")
                         select new
                         {
@@ -33,32 +42,25 @@ namespace ALF_Mine
                 Console.WriteLine("Code {0} has range ({1} - {2}) and description: {3}", code.name, code.minMonth,
                     code.maxMonth, code.desc);
             }
-
-            string configImport = doc.Descendants("configImportDir").First().Value;
-            string defaultExport = doc.Descendants("defaultExportDir").First().Value;
-
-            Console.WriteLine("in: {0}, out {1}", configImport, defaultExport);
         }
 
         //Loads the config and code files in an ara, ara[0] = xmlCode (never null), ara[1] = xmlSettings (can be null).
-        private static XmlDocument[] loadFiles()
+        private static XDocument[] LoadFiles()
         {
-            XmlDocument xmlCodes = null;
-            XmlDocument xmlSettings = null;
-            XmlDocument[] outAra = new XmlDocument[2];
+            XDocument xmlCodes;
+            XDocument xmlSettings = null;
+            XDocument[] outAra = new XDocument[2];
 
-            if (directoryEmpty(@".\import"))
+            if (DirectoryEmpty(@".\import"))
             {
                 if (File.Exists(@".\default\Default_Codes.xml"))
                 {
-                    xmlCodes = new XmlDocument();
-                    xmlCodes.Load(@".\default\Default_Codes.xml");
+                    xmlCodes = XDocument.Load(@".\default\Default_Codes.xml");
                 }
                 else
                 {
-                    createInitCodeXML(@".\default\Default_Codes.xml");
-                    xmlCodes = new XmlDocument();
-                    xmlCodes.Load(@".\default\Default_Codes.xml");
+                    CreateInitCodeXml(@".\default\Default_Codes.xml");
+                    xmlCodes = XDocument.Load(@".\default\Default_Codes.xml");
                 }
 
                 outAra[0] = xmlCodes;
@@ -71,55 +73,39 @@ namespace ALF_Mine
 
                 foreach (string filePath in files)
                 {
-                    XmlDocument temp = new XmlDocument();
-                    temp.Load(filePath);
-                    XmlNodeList nodeList = temp.GetElementsByTagName("configuration");
-                    string type = null;
-                    try
-                    {
-                        XmlNode node = nodeList.Item(0);
-                        type = node.Attributes["type"].Value;
-                    }
-                    catch (Exception e)
-                    {
-
-                    }
-
-                    switch (type)
+                    XDocument doc = XDocument.Load(filePath);
+                    string docType = doc.Element("configuration")?.Attribute("type")?.ToString();
+                    switch (docType)
                     {
                         case "codes":
-                            codesPath = mostRecentFile(codesPath, filePath);
+                            codesPath = MostRecentFile(codesPath, filePath);
                             break;
                         case "settings":
-                            settingsPath = mostRecentFile(settingsPath, filePath);
+                            settingsPath = MostRecentFile(settingsPath, filePath);
                             break;
                     }
                 }
 
                 if (codesPath != null)
                 {
-                    xmlCodes = new XmlDocument();
-                    xmlCodes.Load(codesPath);
+                    xmlCodes = XDocument.Load(codesPath);
                 }
                 else
                 {
                     if (File.Exists(@".\default\Default_Codes.xml"))
                     {
-                        xmlCodes = new XmlDocument();
-                        xmlCodes.Load(@".\default\Default_Codes.xml");
+                        xmlCodes = XDocument.Load(@".\default\Default_Codes.xml");
                     }
                     else
                     {
-                        createInitCodeXML(@".\default\Default_Codes.xml");
-                        xmlCodes = new XmlDocument();
-                        xmlCodes.Load(@".\default\Default_Codes.xml");
+                        CreateInitCodeXml(@".\default\Default_Codes.xml");
+                        xmlCodes = XDocument.Load(@".\default\Default_Codes.xml");
                     }
                 }
 
                 if (settingsPath != null)
                 {
-                    xmlSettings = new XmlDocument();
-                    xmlSettings.Load(settingsPath);
+                    xmlSettings = XDocument.Load(settingsPath);
                 }
 
                 outAra[0] = xmlCodes;
@@ -135,51 +121,53 @@ namespace ALF_Mine
 
 
 
-        private static string mostRecentFile(string current, string toTest)
+        private static string MostRecentFile(string current, string toTest)
         {
             if (current == null)
             {
                 return toTest;
             }
-            else
+
+            DateTime curTime = File.GetCreationTime(current);
+            DateTime testTime = File.GetCreationTime(toTest);
+            if (DateTime.Compare(curTime, testTime) > 0)
             {
-                DateTime curTime = File.GetCreationTime(current);
-                DateTime testTime = File.GetCreationTime(toTest);
-                if (DateTime.Compare(curTime, testTime) > 0)
-                {
-                    return current;
-                }
-                else
-                {
-                    return toTest;
-                }
+                return current;
             }
+
+            return toTest;
         }
 
-        private static void createInitFolders()
+        private static void CreateInitFolders()
         {
             //This must be run on program init
-            DirectoryInfo import = Directory.CreateDirectory(@".\import");
-            DirectoryInfo export = Directory.CreateDirectory(@".\export");
-            DirectoryInfo defaultFolder = Directory.CreateDirectory(@".\default");
+            Directory.CreateDirectory(@".\import");
+            Directory.CreateDirectory(@".\export");
+            Directory.CreateDirectory(@".\default");
             String defPath = @".\default\Default_Codes.xml";
             if (!File.Exists(defPath))
             {
-                createInitCodeXML(defPath);
+                CreateInitCodeXml(defPath);
             }
         }
 
-        private static bool directoryEmpty(string path)
+        private static bool DirectoryEmpty(string path)
         {
             return !Directory.EnumerateFileSystemEntries(path).Any();
         }
 
         //Writes current application settings to folder, either given or default path
-        private static void exportCurrentConfiguration(string path)
+        private static void ExportCurrentSettings(string path)
         {
-            String fileName = "Config_Export_" + DateTime.Now;
+            String fileName = "Settings_Export_" + DateTime.Now + ".xml";
 
-            XmlWriter writer = null;
+            //TODO write application settings
+            XDocument doc = new XDocument(
+                new XElement("configuration", new XAttribute("type", "settings"),
+                    new XElement("customImportDir", ""),
+                    new XElement("customExportDir", "")
+                )
+            );
 
             if (path != null)
             {
@@ -187,82 +175,82 @@ namespace ALF_Mine
                 {
                     FileAttributes attr = File.GetAttributes(path);
 
+                    //If path is just a dir
                     if (attr.HasFlag(FileAttributes.Directory))
                     {
-                        writer = XmlWriter.Create(path);
+                        if (path.EndsWith(@"\"))
+                        {
+                            doc.Save(path + fileName);
+                        }
+                        else
+                        {
+                            doc.Save(path + @"\" + fileName);
+                        }
+                    }
+
+                    //Check if is .xml path then save
+                    String ext = Path.GetExtension(path);
+                    if (ext.Equals(".xml", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        doc.Save(path);
+                    }
+                    else
+                    {
+                        doc.Save(@".\export\" + fileName);
                     }
 
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    writer = XmlWriter.Create(@".\export\" + fileName);
+                    doc.Save(@".\export\" + fileName);
                 }
             }
             else
             {
-                writer = XmlWriter.Create(@".\export\" + fileName);
-            }
-
-            if (writer != null)
-            {
-
-                writer.WriteStartDocument();
-
-                //Write current application settings here
-
-                writer.WriteEndDocument();
-                writer.Flush();
+                doc.Save(@".\export\" + fileName);
             }
 
         }
 
         //Creates initial code definitons to path
-        private static void createInitCodeXML(string path)
+        private static void CreateInitCodeXml(string path)
         {
-            using (XmlWriter writer = XmlWriter.Create(path))
-            {
-                writer.WriteStartDocument();
-                writer.WriteStartElement("configuration");
-                writer.WriteAttributeString("type", "codes");
-                writer.WriteStartElement("codes");
-                writer.WriteStartElement("code");
-                writer.WriteElementString("name", "NO");
-                writer.WriteElementString("desc", "Current licensed ALF with no deficiencies or consultation-only on current visit.");
-                writer.WriteElementString("minMonth", "16");
-                writer.WriteElementString("maxMonth", "18");
-                writer.WriteEndElement();
-                writer.WriteStartElement("code");
-                writer.WriteElementString("name", "NO24");
-                writer.WriteElementString("desc", "Current licensed ALF that has had three consecutive inspections with no written notice of violations and has received no written notice of violations resulting from complaint investigation during that same time period");
-                writer.WriteElementString("minMonth", "16");
-                writer.WriteElementString("maxMonth", "24");
-                writer.WriteEndElement();
-                writer.WriteStartElement("code");
-                writer.WriteElementString("name", "ENF");
-                writer.WriteElementString("desc", "Current licensed ALF with enforcement (fines, stop placement, conditions, revocation, summary suspension) in the past year for either full licensing inspection or complaint investigation.");
-                writer.WriteElementString("minMonth", "9");
-                writer.WriteElementString("maxMonth", "12");
-                writer.WriteEndElement();
-                writer.WriteStartElement("code");
-                writer.WriteElementString("name", "YES");
-                writer.WriteElementString("desc", "Current licensed ALF with deficiencies on current visit that did not result in enforcement.");
-                writer.WriteElementString("minMonth", "13");
-                writer.WriteElementString("maxMonth", "15");
-                writer.WriteEndElement();
-                writer.WriteStartElement("code");
-                writer.WriteElementString("name", "CHOWN");
-                writer.WriteElementString("desc", "Change of ownership (CHOW) with a new licensee.");
-                writer.WriteElementString("minMonth", "6");
-                writer.WriteElementString("maxMonth", "9");
-                writer.WriteEndElement();
-                writer.WriteEndElement();
-                writer.WriteElementString("configImportDir", "");
-                writer.WriteElementString("defaultExportDir", "");
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
-                writer.Flush();
-            }
+            XDocument doc = new XDocument(
+                new XElement("configuration", new XAttribute("type", "codes"),
+                    new XElement("codes",
+                        new XElement("code",
+                            new XElement("name", "NO"),
+                            new XElement("desc", "Current licensed ALF with no deficiencies or consultation-only on current visit."),
+                            new XElement("minMonth", "16"),
+                            new XElement("maxMonth", "18")),
+                        new XElement("code",
+                            new XElement("name", "NO24"),
+                            new XElement("desc", "Current licensed ALF that has had three consecutive inspections with no written notice of violations and has received no written notice of violations resulting from complaint investigation during that same time period."),
+                            new XElement("minMonth", "16"),
+                            new XElement("maxMonth", "24")),
+                        new XElement("code",
+                            new XElement("name", "ENF"),
+                            new XElement("desc", "Current licensed ALF with enforcement (fines, stop placement, conditions, revocation, summary suspension) in the past year for either full licensing inspection or complaint investigation."),
+                            new XElement("minMonth", "9"),
+                            new XElement("maxMonth", "12")),
+                        new XElement("code",
+                            new XElement("name", "YES"),
+                            new XElement("desc", "Current licensed ALF with deficiencies on current visit that did not result in enforcement."),
+                            new XElement("minMonth", "13"),
+                            new XElement("maxMonth", "15")),
+                        new XElement("code",
+                            new XElement("name", "CHOWN"),
+                            new XElement("desc", "Change of ownership (CHOW) with a new licensee."),
+                            new XElement("minMonth", "6"),
+                            new XElement("maxMonth", "9")
+                        )
+                    )
+                )
+            );
+            doc.Save(path);
         }
+
+
 
     }
 }
