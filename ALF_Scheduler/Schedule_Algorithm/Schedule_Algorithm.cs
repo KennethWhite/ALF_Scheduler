3c19b5c3e7cc5c3e1c6dc27ea9bd43b3b217f953
Brandtly Strobeck <bstrobeck@gmail.com>
2019-02-05 21:36:42 -0800

    Added upper limit checking on codes, and doctored up the alterAverage()

--- ALF_Scheduler/Schedule_Algorithm/Schedule_Algorithm.cs
+++ ALF_Scheduler/Schedule_Algorithm/Schedule_Algorithm.cs
@@ -13,22 +13,23 @@ namespace ScheduleGenerator
         {
             Console.WriteLine("Enter number of inspections to run: ");
             int choice = Convert.ToInt32(Console.ReadLine());
+            List<String> codeList = new List<String>();
             List<DateTime> lastVisit = new List<DateTime>();
             List<DateTime> nextVisit = new List<DateTime>();
-            nextVisit = autoRun(lastVisit, choice);
+            nextVisit = autoRun(lastVisit, choice, codeList);
             Console.WriteLine("Next Visit Dates Pre alterAverage(): ");
-            foreach (DateTime d in nextVisit)
-                printDates(d);
-
-
 
             double average = calcAverage(nextVisit);
             int i = 0;
             Console.WriteLine(i + ". Average: " + average);
-            if (average < 15.99)
+            while(average < 15.9 || average == 16 || average > 15.9)
             {
+                if (i>=999)
+                    break;
+
+
                 i++;
-                average = alterAverage(nextVisit);
+                average = alterAverage(nextVisit, codeList, lastVisit);
                 Console.WriteLine(i + ". Average: " + average);
             }
 
@@ -38,27 +39,42 @@ namespace ScheduleGenerator
                 Console.WriteLine("----> Next Visit: " + nextVisit[j]);
             }
             Console.WriteLine("Final Average: " + calcAverage(nextVisit));
-
         }
 
-        public static double alterAverage(List<DateTime> nextVisit)//idea is to loop through the List of dates, trying to spread out the days in between visits to make the average go up.
+        public static double alterAverage(List<DateTime> nextVisit,List<String> codeList, List<DateTime> lastVisit)//idea is to loop through the List of dates, trying to spread out the days in between visits to make the average go up.
         {
             //on averagee there is 30.42 days in a month.
             double avg = calcAverage(nextVisit);
             double diff = avg - 15.99;//calculate the difference between the average we have and 15.99. Not sure if it is needed, but for right now its good to have an idea of how far off target it is.
             double diffInDays = Math.Abs(diff) * 30.42;
             double diffInDaysDividedByCount = diffInDays / nextVisit.Count();
-
+            if(diffInDaysDividedByCount > 0 && diffInDaysDividedByCount < 1)//if the diffInDaysDiviedByCount is a decimal, then it is small enough to call it quits.
+            {
+                return 0.0;
+            }
             Console.WriteLine("diff in days: " + diffInDays);
             Console.WriteLine("diff in days / List Count = " + diffInDaysDividedByCount);
 
             if (diff < 0)//checking for if we need to add more days in order to raise the averaeg. Will need a similar check later to see if average is above 15.99 too.
             {
-                for (int i = 0; i < nextVisit.Count; i++)
+                for (int i = 1; i < nextVisit.Count; i++)
                 {
-                    nextVisit[i] = nextVisit[i].AddDays(diffInDaysDividedByCount);//Code in here is what is wonky. Need to figure out how to spread the visits apart more, while still maintaining WA Guidelines.
-                    //Console.Write("Altered List: ");
-                    //printDates(nextVisit[i]);
+                    Console.WriteLine("Pre Avg Alter: " + nextVisit[i]);
+                    int codeCheck = (int)(nextVisit[i] - lastVisit[i]).TotalDays;
+                    if (checkCodeBound(codeList[i],codeCheck)== true)
+                    {
+                        nextVisit[i] = nextVisit[i].AddDays(diffInDaysDividedByCount);//Code in here is what is wonky. Need to figure out how to spread the visits apart more, while still maintaining WA Guidelines.
+                    }                        
+                    Console.WriteLine("Post Avg Alter: " + nextVisit[i]);
+                }
+            }
+            else
+            {
+                for (int i = 1; i < nextVisit.Count; i++)
+                {
+                    Console.WriteLine("Pre Avg Alter: " + nextVisit[i]);
+                    nextVisit[i] = nextVisit[i].AddDays(-diffInDaysDividedByCount);//Code in here is what is wonky. Need to figure out how to spread the visits apart more, while still maintaining WA Guidelines.
+                    Console.WriteLine("Post Avg Alter: " + nextVisit[i]);
                 }
             }
             foreach (DateTime d in nextVisit)
@@ -67,14 +83,15 @@ namespace ScheduleGenerator
             }
             return calcAverage(nextVisit);//calc new average
         }
+
         public static double calcAverage(List<DateTime> myDates)//https://dotnetcodr.com/2015/10/30/calculate-the-number-of-months-between-two-dates-with-c/
         {
             double average = 0;
             int timeTweenMonths = 0;
 
-            for (int i = 0; i < myDates.Count - 1; i++)
+            for (int i = 0; i < myDates.Count -1; i++)
             {
-                timeTweenMonths = 12 * (myDates[i].Year - myDates[i + 1].Year) + myDates[i].Month - myDates[i + 1].Month;//calculations here borrowed from link on method header line.
+                timeTweenMonths = 12 * (myDates[i].Year - myDates[i + 1].Year) + myDates[i].Month - myDates[i + 1].Month + 1;//calculations here borrowed from link on method header line.
                 average += timeTweenMonths;
             }
             average = average / myDates.Count;
@@ -82,7 +99,7 @@ namespace ScheduleGenerator
             return Math.Abs(average);
         }
 
-        public static List<DateTime> autoRun(List<DateTime> lastVisit, int runs)
+        public static List<DateTime> autoRun(List<DateTime> lastVisit, int runs, List<String> codeList)
         {
             Calendar myCal = CultureInfo.InvariantCulture.Calendar;
             DateTime myDt = new DateTime();//this will be the prevous inspection date
@@ -103,6 +120,7 @@ namespace ScheduleGenerator
                 Console.WriteLine("Enter Result Code of Inspection: ");
                 resultCode = Console.ReadLine();
                 verifyCode = isCode(resultCode, myDt);
+                codeList.Add(resultCode);
                 lastVisit.Add(myDt);
 
                 futureDate = myDt.AddDays(verifyCode);
@@ -127,13 +145,8 @@ namespace ScheduleGenerator
             int toReturn = rng.Next(low, high);
             DateTime highTempDate;
             DateTime lowTempDate;
-            //Console.WriteLine("==========================");
-            //Console.WriteLine("Range of days: " + low + " - " + high);
-            //Console.WriteLine("Number generated from range: " + toReturn);
-
             lowTempDate = pastInspection.AddDays(low);
             highTempDate = pastInspection.AddDays(high);
-            //Console.WriteLine("Range of possible inspection days based off inspection code: " + lowTempDate + " - " + highTempDate);
             Console.WriteLine("==========================");
             return toReturn;
         }
@@ -155,7 +168,28 @@ namespace ScheduleGenerator
                 Console.WriteLine("Invalid Code.");
                 return 0; //input error
             }
-            //hello world
+        }
+
+        public static bool checkCodeBound(String code, int diff)//checking to see if the future scheduled date goes beyond the highest bound of the corresponding code.
+        {
+            if (code.CompareTo("NO24") == 0)
+            {
+                return (730 - diff) > 0;
+            }
+            else if (code.CompareTo("NO") == 0)
+            {
+                return (547 - diff) > 0;//16-18 months in days
+
+            }
+            else if (code.CompareTo("YES") == 0)
+                return (456 - diff) > 0;//13-15 months in days
+            else if (code.CompareTo("ENF") == 0)
+                return (365 - diff) > 0;//9-12 months in days
+            else if (code.CompareTo("CHOWN") == 0)
+                return (273 - diff) > 0;//6-9 months in days
+
+            else
+                return true;
         }
 
     }
---
+++ ALF_Scheduler/Schedule_Algorithm/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScheduleGenerator;
+
+namespace ScheduleGenerater
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            ScheduleAlgorithm.start();
+            
+        }
+    }
+}
