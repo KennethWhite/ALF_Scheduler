0af3eddad1549c9cfa1486fa7610a0df016804fd
Colton Johansson <cjohansson@eagles.ewu.edu>
2019-02-25 13:08:38 -0800

    Revert "Revert "Added xml documentation""
    This reverts commit af35d5d958291e5cc4ff853587f64996ca8c0b70.

--- ALF_Scheduler/XML_Utils/XML_Utils.cs
+++ ALF_Scheduler/XML_Utils/XML_Utils.cs
@@ -7,24 +7,10 @@ namespace XML_Utils
 {
     public static class XML_Utils
     {
-        //Just here to show how to use Linq with XDocument
-        public static void ReadCodeXml(XDocument doc)
-        {
-            var codes = from c in doc.Descendants("code")
-                select new
-                {
-                    name = c.Descendants("name").First().Value,
-                    desc = c.Descendants("desc").First().Value,
-                    minMonth = c.Descendants("minMonth").First().Value,
-                    maxMonth = c.Descendants("maxMonth").First().Value
-                };
-
-            foreach (var code in codes)
-                Console.WriteLine("Code {0} has range ({1} - {2}) and description: {3}", code.name, code.minMonth,
-                    code.maxMonth, code.desc);
-        }
-
-        //Returns the preferred import directory. Default is .\import, otherwise gets from settings file
+        /// <summary>
+        /// Returns the preferred import directory. Default is .\import, otherwise gets from settings file
+        /// </summary>
+        /// <returns></returns>
         public static string GetImportDir()
         {
             try
@@ -44,7 +30,10 @@ namespace XML_Utils
             return @".\import";
         }
 
-        //Returns the preferred export directory. Default is .\export, otherwise gets from settings file
+        /// <summary>
+        /// Returns the preferred export directory. Default is .\export, otherwise gets from settings file
+        /// </summary>
+        /// <returns></returns>
         public static string GetExportDir()
         {
             var settingsDoc = LoadSettingFile();
@@ -57,7 +46,10 @@ namespace XML_Utils
             return @".\export";
         }
 
-        //Checks if settings file exists in default directory. If we choose to implement settings file, there will need to be a settings file here by default to at least point to custom import directories, to load imported settings.
+        /// <summary>
+        /// Checks if settings file exists in default directory. If we choose to implement settings file, there will need to be a settings file here by default to at least point to custom import directories, to load imported settings.
+        /// </summary>
+        /// <returns></returns>
         public static string DefaultSettingsFileExist()
         {
             try
@@ -70,7 +62,10 @@ namespace XML_Utils
             }
         }
 
-        //Gets a settings file from where ever one is located, either default directory, or from custom import dir
+        /// <summary>
+        /// Gets a settings file from where ever one is located, either default directory, or from custom import dir
+        /// </summary>
+        /// <returns></returns>
         public static XDocument LoadSettingFile()
         {
             var settingsPath = DefaultSettingsFileExist();
@@ -99,7 +94,12 @@ namespace XML_Utils
             throw new FileNotFoundException("No root settings file in default directory");
         }
 
-        //Gets path of most recent config file given a directory to search and a type of config xml
+        /// <summary>
+        /// Gets path of most recent config file given a directory to search and a type of config xml
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public static string GetConfigPathFromDir(string directory, string type)
         {
             var files = Directory.GetFiles(directory, "*.xml", SearchOption.TopDirectoryOnly);
@@ -115,7 +115,10 @@ namespace XML_Utils
             throw new FileNotFoundException("Could not find settings file in directory specified");
         }
 
-        //Will load codes file from default path or if settings file contains a customImportDir, go to that dir and check if there is codes xml
+        /// <summary>
+        /// Will load codes file from default path or if settings file contains a customImportDir, go to that dir and check if there is codes xml
+        /// </summary>
+        /// <returns></returns>
         public static XDocument LoadCodeFile()
         {
             var importDir = GetImportDir();
@@ -145,7 +148,12 @@ namespace XML_Utils
             return XDocument.Load(codePath);
         }
 
-        //Returns the path of the string whose file was last written to.
+        /// <summary>
+        /// Returns the path of the string whose file was last written to.
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="toTest"></param>
+        /// <returns></returns>
         private static string MostRecentFile(string current, string toTest)
         {
             if (current == null) return toTest;
@@ -157,7 +165,9 @@ namespace XML_Utils
             return toTest;
         }
 
-        //Should be run on program start. Creates neccessary files/folders for proper execution
+        /// <summary>
+        /// Should be run on program start. Creates neccessary files/folders for proper execution
+        /// </summary>
         public static void CreateInitFolders()
         {
             //This must be run on program init
@@ -177,7 +187,10 @@ namespace XML_Utils
             }
         }
 
-        //Generates a settings file with no custom data in it
+        /// <summary>
+        /// Generates a settings file with no custom data in it
+        /// </summary>
+        /// <param name="path"></param>
         public static void CreateGenericSettingsFile(string path)
         {
             var doc = new XDocument(
@@ -190,7 +203,10 @@ namespace XML_Utils
             doc.Save(path + @"\Default_Settings.xml");
         }
 
-        //Writes current application settings to folder, either given or default path. Can be given folder or .xml path.
+        /// <summary>
+        /// Writes current application settings to folder, either given or default path. Can be given folder or .xml path.
+        /// </summary>
+        /// <param name="path"></param>
         public static void ExportCurrentSettings(string path)
         {
             var fileName = "Settings_Export_" + DateTime.Now + ".xml";
@@ -232,7 +248,10 @@ namespace XML_Utils
                 doc.Save(@".\export\" + fileName);
         }
 
-        //Creates initial code definitons xml file to specified path
+        /// <summary>
+        /// Creates initial code definitons xml file to specified path
+        /// </summary>
+        /// <param name="path"></param>
         public static void CreateInitCodeXml(string path)
         {
             var doc = new XDocument(
